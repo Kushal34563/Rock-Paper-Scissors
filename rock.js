2ef0dfa9e0f49bf77a28c06f9ac57ed5828f7f02
Kushal <kushal34563@gmail.com>
2022-04-21 17:09:24 +0530

    Edit

--- rock.js
+++ rock.js
@@ -3,7 +3,7 @@ let cs = 0
 const buttons = document.querySelectorAll('input')
 
 function computerPlay(){
-    var things = ['Rock', 'Paper', 'Scissor'];
+    var things = ['rock', 'paper', 'scissors'];
     var thing = things[Math.floor(Math.random()*things.length)];
     return thing
 }
@@ -13,22 +13,20 @@ function playRound(playerSelection) {
     let computerSelection = computerPlay();
     let result = ""
 
-    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
-    (playerSelection == 'scissors' && computerSelection == 'paper') ||
-    (playerSelection == 'paper' && computerSelection == 'rock')) {
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'scissors' && computerSelection == 'paper') ||(playerSelection == 'paper' && computerSelection == 'rock')) {
     
         ps += 1
-    result = ('win ' + playerSelection + ' beats ' + computerSelection + '<br>pscore:' + ps + '<br>cscore:' + cs)
-        if (playerScore == 5) {
+        result = ('win      ' + playerSelection + ' beats ' + computerSelection + '<br>pscore:' + ps + '<br>cscore:' + cs)
+        if (ps== 5) {
             result += '<br><br>You won the game! Reload the page to play again'
         }
     }
     else if (playerSelection == computerSelection){
-        result = ('tie ' + '<br>pscore:' + ps + '<br>cscore:' + cs)
+        result = ('tie      ' + '<br>pscore:' + ps + '<br>cscore:' + cs)
     }
     else {
         cs += 1
-        result = ('lose ' + playerSelection + ' lost to ' + computerSelection + '<br>pscore:' + ps + '<br>cscore:' + cs)
+        result = ('lose             ' + playerSelection + ' lost to ' + computerSelection + '<br>pscore:' + ps + '<br>cscore:' + cs)
 
         if (cs == 5) {
             result += '<br><br>I won the game! Reload the page to play again'

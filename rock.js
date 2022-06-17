b71655a072c10e7122c87b403cc6af8653f53493
Kushal <kushal34563@gmail.com>
2022-06-17 20:42:30 +0530

    17 June

--- rock.js
+++ rock.js
@@ -16,23 +16,25 @@ function playRound(playerSelection) {
     if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'scissors' && computerSelection == 'paper') ||(playerSelection == 'paper' && computerSelection == 'rock')) {
     
         ps += 1
-        result = ('win      ' + playerSelection + ' beats ' + computerSelection + '<br>pscore:' + ps + '<br>cscore:' + cs)
+        result = ('win<br>      ' + playerSelection + ' beats ' + computerSelection + '<br>p score:' + ps + '<br>c score:' + cs)
         if (ps== 5) {
             result += '<br><br>You won the game! Reload the page to play again'
         }
     }
     else if (playerSelection == computerSelection){
-        result = ('tie      ' + '<br>pscore:' + ps + '<br>cscore:' + cs)
+        result = ('tie      ' + '<br>p score:' + ps + '<br>c score:' + cs)
     }
     else {
         cs += 1
-        result = ('lose             ' + playerSelection + ' lost to ' + computerSelection + '<br>pscore:' + ps + '<br>cscore:' + cs)
+        result = ('lose<br>            ' + playerSelection + ' lost to ' + computerSelection + '<br>p score:' + ps + '<br>c score:' + cs)
 
         if (cs == 5) {
             result += '<br><br>I won the game! Reload the page to play again'
         }
     }
     document.getElementById('result').innerHTML = result
+    document.getElementById('playerc').innerHTML = playerSelection
+    document.getElementById('compc').innerHTML = computerSelection
     return
 }
   
@@ -40,7 +42,23 @@ function playRound(playerSelection) {
 
 
 buttons.forEach(button =>{
-    button.addEventListener('click', function(){
+    button.addEventListener('click', ()=>{
         playRound(button.value)
+    })
+})
+
+buttons.forEach(button =>{
+    
+    button.addEventListener('click', ()=>{
+        const bgc = button.style.backgroundColor;
+        if (bgc === 'green') {
+            button.style.backgroundColor = 'yellow';
+            button.style.color = 'black';
+            
+        } else {
+            button.style.backgroundColor = 'green';
+            button.style.color = 'white';
+        }
+        
     })
 })
